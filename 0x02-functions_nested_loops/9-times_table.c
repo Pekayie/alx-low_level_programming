fb7ce490e99c79f71838f4107daf1f818684907e
Bright Okyere <brightenninokyere@gmail.com>
2022-09-14 16:31:27 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,10 +6,11 @@
 void times_table(void)
 {
 	int num, mult, prod;
-	
+
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
+
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -21,12 +22,10 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-
+			
 			_putchar((prod % 10) + '0');
-
-
-
 		}
 		_putchar('\n');
+
 	}
 }
