dd069faaaec6b56aa5cc66dbcd237f6282ab1ffb
Bright Okyere <brightenninokyere@gmail.com>
2022-09-14 08:28:39 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,19 @@
 #include "main.h"
-
-
-
 /**
-
- * main - check the code.
-
- *
-
- * Return: Always 0.
-
- */
-
-int main(void)
-
+* print_alphabet_x10 - function to print abc 10 times
+*
+* Return: 0
+*/
+void print_alphabet_x10(void)
 {
+	char c, i;
 
-    print_alphabet_x10();
-
-    return (0);
-
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
